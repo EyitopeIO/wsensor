d6dbd946e4039b133b7c68890263f6a23ec13362
EyitopeIO <adeyitope.io@gmail.com>
2021-11-03 18:03:24 +0100

    Included functions for test cases.

---
+++ Makefile
@@ -0,0 +1,18 @@
+
+include $(CONTIKI)/Makefile.include
+
+CONTIKI_PROJECT = wsensor
+
+CFLAGS += --Wall --Wextra 
+
+MODULES += $(CONTIKI_NG_SERVICES_DIR)/unit-test
+
+all: $(CONTIKI_PROJECT)
+
+test_all: eyitope-test-calc.o
+	$(CC) eyitope-test-calc.c -o test_all
+
+eyitope-test-calc.o: eyitope-calc.h eyitope-calc.calc
+
+
+
--- eyitope-calc-test.c
+++ eyitope-calc-test.c
@@ -1,1 +1,37 @@
+#include <assert.h>
+#include <stdio.h>
+#include "eyitope.h"
 
+void test_init() {
+    ringbuffer rb;
+    rb_init(&rb);
+    assert(rb.front == -1);
+    assert(rb.rear == -1);
+    assert(rb.front == rb.rear);
+}
+
+void test_pop() {
+
+}
+
+void test_push() {
+    ringbuffer rb
+}
+
+void test_head() {
+
+}
+
+void test_tail() {
+
+}
+
+void test_peek() {
+
+}
+
+
+int main() {
+    test_init();
+    return 0;
+}
