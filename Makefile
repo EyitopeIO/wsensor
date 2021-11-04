1ffb05db7659c6bc17658d670f92d6953dca5642
EyitopeIO <adeyitope.io@gmail.com>
2021-11-04 13:20:36 +0100

    Corrections to makefile and unit test process.

--- Makefile
+++ Makefile
@@ -4,18 +4,15 @@ CONTIKI_PROJECT = wsensor
 # CFLAGS += --Wall --Wextra 
 
 MODULES += $(CONTIKI_NG_SERVICES_DIR)/unit-test
-PROJECT_SOURCEFILES += eyitope-ring-buffer.c
+# PROJECT_SOURCEFILES += eyitope-ring-buffer.c eyitope-calc-test.c 
+
+TEST_SRC += eyitope-ring-buffer.c eyitope-calc-test.c
+TEST_HDR += eyitope-ring-buffer.h
 
 all: $(CONTIKI_PROJECT)
 
-test-all: eyitope-calc-test.o
-	
-eyitope-calc-test.o: eyitope-ring-buffer.o 
-	$(CC) eyitope-calc-test.c -o test-all
-
-eyitope-ring-buffer.o: eyitope-ring-buffer.h
-	$(CC) eyitope-ring-buffer.c -o eyitope-ring-buffer.o
-
+test-all: $(TEST_SRC) $(TEST_HDR)
+	$(CC) $(TEST_SRC) -o $@
 
 CONTIKI = ../contiki-ng
 include $(CONTIKI)/Makefile.include
--- eyitope-calc-test.c
+++ eyitope-calc-test.c
@@ -1,15 +1,14 @@
 
-#include <stdio.h>
-#include <unit-test.h>
-#include <contiki.h>
+#include "unit-test.h"
+#include "contiki.h"
 #include "eyitope-ring-buffer.h"
 
 UNIT_TEST_REGISTER(test_init, "init test");
-UNIT_TEST_REGISTER(test_pop, "test pop");
-UNIT_TEST_REGISTER(test_push, "test push");
-UNIT_TEST_REGISTER(test_head, "test head");
-UNIT_TEST_REGISTER(test_tail, "test tail");
-UNIT_TEST_REGISTER(test_peek, "test peek");
+// UNIT_TEST_REGISTER(test_pop, "test pop");
+// UNIT_TEST_REGISTER(test_push, "test push");
+// UNIT_TEST_REGISTER(test_head, "test head");
+// UNIT_TEST_REGISTER(test_tail, "test tail");
+// UNIT_TEST_REGISTER(test_peek, "test peek");
 
 
 UNIT_TEST(test_init) 
@@ -26,36 +25,36 @@ UNIT_TEST(test_init)
     UNIT_TEST_END();
 }
 
-UNIT_TEST(test_pop) 
-{
-    UNIT_TEST_BEGIN();
-    UNIT_TEST_END();
-}
+// UNIT_TEST(test_pop) 
+// {
+//     UNIT_TEST_BEGIN();
+//     UNIT_TEST_END();
+// }
 
-UNIT_TEST(test_push) 
-{
-    ringbuffer rb;
-    UNIT_TEST_BEGIN();
-    UNIT_TEST_END();
-}
+// UNIT_TEST(test_push) 
+// {
+//     ringbuffer rb;
+//     UNIT_TEST_BEGIN();
+//     UNIT_TEST_END();
+// }
 
-UNIT_TEST(test_head) 
-{
-    UNIT_TEST_BEGIN();
-    UNIT_TEST_END();
-}
+// UNIT_TEST(test_head) 
+// {
+//     UNIT_TEST_BEGIN();
+//     UNIT_TEST_END();
+// }
 
-UNIT_TEST(test_tail) 
-{
-    UNIT_TEST_BEGIN();
-    UNIT_TEST_END();
-}
+// UNIT_TEST(test_tail) 
+// {
+//     UNIT_TEST_BEGIN();
+//     UNIT_TEST_END();
+// }
 
-UNIT_TEST(test_peek) 
-{
-    UNIT_TEST_BEGIN();
-    UNIT_TEST_END();
-}
+// UNIT_TEST(test_peek) 
+// {
+//     UNIT_TEST_BEGIN();
+//     UNIT_TEST_END();
+// }
 
 
 PROCESS(test_all, "Unit test for all");
