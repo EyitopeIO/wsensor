53f2d9b0a8c2c275a18d4117127acfbd91833fec
EyitopeIO <adeyitope.io@gmail.com>
2021-11-08 03:18:50 +0100

    Little bug fix.

--- eyitope-calc.c
+++ eyitope-calc.c
@@ -12,20 +12,19 @@ struct tracker* osw_average(queue_t tomato_stack)
     float total = 0.0f;
     float phead = 0.0f;
 
-    int i = 6;
-    int b = 0;
+    int i = 0;
 
     while(i--) x[i] = 0;
 
-    for(i = 6; ((p = queue_dequeue(tomato_stack)) != NULL) && i >= 0; i-- ) {
-        b = abs(i - 6);
+    for(i = 1; ((p = queue_dequeue(tomato_stack)) != NULL) && i < 8; i++ ) {
+        // b = abs(i - 6);
         total += p->reading ;
-        x[abs(b)] = total / (b + 1);
-        printf("rd: %f, to: %f, av: %f\n", p->reading, total, total/b);
+        x[i - 1] = total / i;
+        printf("rd: %f, to: %f, av: %f\n, i: %d", p->reading, total, total/i, i);
     }
-    for(i = 0; i < 6; i++) printf("x[%d] = %f\n", i, x[i]);
+    for(i = 0; i < 7; i++) printf("x[%d] = %f\n", i, x[i]);
 
-    t.index = b;
+    t.index = i;
     t.data = x;
     return &t;
 }
