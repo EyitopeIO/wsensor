9ba24fdc77cdf46aca2881dce6d19147ca484461
EyitopeIO <adeyitope.io@gmail.com>
2021-11-08 01:43:37 +0100

    Trying out built-in queue library. Bug fix.

--- eyitope-calc-test.c
+++ eyitope-calc-test.c
@@ -48,7 +48,7 @@ UNIT_TEST_REGISTER(test_osw, "Calculation test");
 //         UNIT_TEST_ASSERT(zr[i] == cfm[i]);
 //     }
 //     UNIT_TEST_END();
-}
+
 
 UNIT_TEST(test_osw) 
 {
@@ -58,9 +58,37 @@ UNIT_TEST(test_osw)
     float *p = NULL;
     float cfm[] = {5.0f, 5.5f, 6.0f, 6.5f, 7.0f, 7.5f, 8.0f, 9.0};
 
-    queue_init(sensorval);
-    for (a = 5.0f; (queue_enqueue(sensorval, &a) != -1) && (a < 12.0f); a++);
-    while ((p = queue_dequeue(sensorval)) != NULL) printf("dq: %f\n", (float)(p->reading));
+    queue_t worm;
+    queue_init(worm);
+
+    struct sensorval sv1;
+    struct sensorval sv2; 
+    struct sensorval sv3; 
+    struct sensorval sv4; 
+    struct sensorval sv5; 
+    struct sensorval sv6; 
+    struct sensorval sv7;
+    struct sensorval sv8; 
+
+    sv1.reading = a + 1.0;
+    sv2.reading = a + 2.0;
+    sv3.reading = a + 3.0;
+    sv4.reading = a + 4.0;
+    sv5.reading = a + 5.0;
+    sv6.reading = a + 6.0;
+    sv7.reading = a + 7.0;
+    sv8.reading = a + 8.0;
+
+    queue_enqueue(worm, &sv1);
+    queue_enqueue(worm, &sv2);
+    queue_enqueue(worm, &sv3);
+    queue_enqueue(worm, &sv4);
+    queue_enqueue(worm, &sv5);
+    queue_enqueue(worm, &sv6);
+    queue_enqueue(worm, &sv7);
+    queue_enqueue(worm, &sv8);
+    
+    while ((p = queue_dequeue(worm)) != NULL) printf("dq: %f\n", (float)(p->reading));
 
     UNIT_TEST_BEGIN();
     UNIT_TEST_ASSERT(1+1 == 2);
