1e5385381d41434ca5c242acc8bc62d8fcc41097
EyitopeIO <adeyitope.io@gmail.com>
2021-11-08 02:26:10 +0100

    Little bug fix.

--- eyitope-calc.c
+++ eyitope-calc.c
@@ -1,6 +1,7 @@
 #include "eyitope-calc.h"
 #include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
 
 struct tracker* osw_average(queue_t tomato_stack)
 {
@@ -10,15 +11,18 @@ struct tracker* osw_average(queue_t tomato_stack)
     struct sensorval *p; 
     float total = 0.0f;
     int i = 6;
+    int b = 0;
 
     while(i--) x[i] = 0;
 
     for(i = 7; ((p = queue_dequeue(tomato_stack)) != NULL) && i--; ) {
+        b = abs(i - 7);
         total += p->reading;
-        x[abs(i-7)] = total / abs(i-7);
+        printf("reading: %f, total: %f\n", p->reading, total);
+        x[abs(b)] = total / abs(b);
     }
 
-    t.index = i;
+    t.index = b;
     t.data = x;
     return &t;
 }
