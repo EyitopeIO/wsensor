6853d35e1d9b47a0568231227a62ebbb2a12baa3
EyitopeIO <adeyitope.io@gmail.com>
2021-11-11 20:12:31 +0100

    More additions. See previous commit

--- eyitope.c
+++ eyitope.c
@@ -3,22 +3,24 @@
 #include "eyitope-calc.h"
 #include "dev/sensor/sht11/sht11.h"
 #include "dev/sensor/sht11/sht11-sensor.h"
+#include "sys/timer.h"
 
 PROCESS(sense_and_send, "Main process");
 
-PROCESS_THREAD(test_all, ev, data) 
+AUTOSTART_PROCESSES(&sense_and_send);
+
+PROCESS_THREAD(sense_and_send, ev, data) 
 {
-    struct sensorval_l hu_r;    // humidity readings
-    struct sensorval_l te_r;    // temperature readings
+    LIST(quantum_tunnel_h);     // List of humidity readings
+    LIST(quantum_tunnel_t);     // List of temperature readings
+    static struct sensorval_l hu_r;    // Humidity readings
+    static struct sensorval_l te_r;    // Temperature readings
+    static struct etimer time_to_read;
 
     PROCESS_BEGIN();
 
-    LIST(quantum_tunnel);
-    list_init(quantum_tunnel);
-
-
-
-
+    list_init(quantum_tunnel_h);
+    list_init(quantum_tunnel_t);
 
     PROCESS_END();
 }
