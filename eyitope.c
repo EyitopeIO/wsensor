1fb54317036dbc906f67d3755a381a875d8d0c98
EyitopeIO <adeyitope.io@gmail.com>
2021-11-12 18:00:59 +0100

    Using semi-static memory to save RAM

--- eyitope.c
+++ eyitope.c
@@ -9,7 +9,7 @@
 #include "stdio.h"
 #include "random.h"
 #include "math.h"
-#include "heapmem.h"
+#include "memb.h"
 
 PROCESS(sense_and_send, "Main process");
 
@@ -18,10 +18,12 @@ AUTOSTART_PROCESSES(&sense_and_send);
 PROCESS_THREAD(sense_and_send, ev, data) 
 {
 
-    static struct sensorval_l *hu_r;    // Humidity readings
-    static struct sensorval_l *te_r;    // Temperature readings
-    static struct sensorval_l *hu_p;
-    static struct sensorval_l *te_p;
+    static struct sensorval_l *th_r;    // Humidity readings
+    // static struct sensorval_l *te_r;    // Temperature readings
+    // static struct sensorval_l *hu_p;
+    // static struct sensorval_l *te_p;
+    static struct sensorval_l *th_p;
+    
 
     static struct etimer time_to_read;
     static float avr_h;
@@ -30,17 +32,32 @@ PROCESS_THREAD(sense_and_send, ev, data)
 
     PROCESS_BEGIN();
 
-    LIST(quantum_tunnel_h);     // List of humidity readings
-    LIST(quantum_tunnel_t);     // List of temperature readings
+    // LIST(quantum_tunnel_h);     // List of humidity readings
+    // LIST(quantum_tunnel_t);     // List of temperature readings
+    LIST(quantum_l);
+    MEMB(th_buff, struct sensorval_l, WINDOW_SIZE);
 
     random_init(3);
 
-    list_init(quantum_tunnel_h);
-    list_init(quantum_tunnel_t);
+    // list_init(quantum_tunnel_h);
+    // list_init(quantum_tunnel_t);
+    list_init(quantum_l);
+    memb_init(&th_buff);
 
-    hu_p = (struct sensorval_l*)heapmem_alloc(WINDOW_SIZE * sizeof(struct sensorval_l));
-    te_p = (struct sensorval_l*)heapmem_alloc(WINDOW_SIZE * sizeof(struct sensorval_l));
-    if (hu_p == NULL || te_p == NULL) {
+    th_p = (struct sensorval_l*)memb_alloc(&th_buff);
+
+    // hu_p = (struct sensorval_l*)heapmem_alloc(WINDOW_SIZE * sizeof(struct sensorval_l));
+    // te_p = (struct sensorval_l*)heapmem_alloc(WINDOW_SIZE * sizeof(struct sensorval_l));
+    // hu_p = (struct sensorval_l*)heapmem_alloc(WINDOW_SIZE * sizeof(struct sensorval_l));
+    // te_p = (struct sensorval_l*)heapmem_alloc(WINDOW_SIZE * sizeof(struct sensorval_l));
+
+    // if (hu_p == NULL || te_p == NULL) {
+    //     printf("Not enough memory!\n");
+    //     PROCESS_EXIT();
+    // }
+
+
+    if (th_p == NULL) {
         printf("Not enough memory!\n");
         PROCESS_EXIT();
     }
@@ -53,30 +70,38 @@ PROCESS_THREAD(sense_and_send, ev, data)
     
     printf("Sensor activated!\n");
 
-
     while(1) {
 
         printf("Loop!\n");
 
-        hu_r = hu_p;
-        te_r = te_p;
-        counter = WINDOW_SIZE;
+        // hu_r = hu_p;
+        // te_r = te_p;
 
+        counter = WINDOW_SIZE;
+        th_r = th_p;
         while (counter--) { 
             PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&time_to_read));
             // hu_r->reading = (float)sht11_sensor.value(SHT11_SENSOR_HUMIDITY);
             // te_r->reading = (float)sht11_sensor.value(SHT11_SENSOR_TEMP);
-            hu_r->reading = (float)random_rand();
-            te_r->reading = (float)random_rand();
-            list_add(quantum_tunnel_h, hu_r++);
-            list_add(quantum_tunnel_t, te_r++);
+            th_r->reading = (float)random_rand();
+            // te_r->reading = (float)random_rand();
+            // list_add(quantum_tunnel_h, hu_r++);
+            // list_add(quantum_tunnel_t, te_r++);
+            list_add(&quantum_l, th_r++);
             etimer_reset(&time_to_read);
         }
-
-        avr_h = (0.01 * osw_average(&quantum_tunnel_h)) - 36.9;
+        avr_h = (0.01 * osw_average(&quantum_l)) - 36.9;
         avr_h = (0.01 * avr_h) - 36.9;
 
-        avr_t = osw_average(&quantum_tunnel_t);
+        counter = WINDOW_SIZE;
+        th_r = th_p;
+        while (counter--) { 
+            PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&time_to_read));
+            th_r->reading = (float)random_rand();
+            list_add(&quantum_l, th_r++); 
+            etimer_reset(&time_to_read);
+        }
+        avr_t = osw_average(&quantum_l);
         avr_t = -4 + (0.0405 * avr_t) - ((2.8 * pow(10, -6)) * pow(avr_t, 2));
 
         printf("avg humidity: %f\n", avr_h);
@@ -86,8 +111,10 @@ PROCESS_THREAD(sense_and_send, ev, data)
 
     }
 
-    heapmem_free(hu_p);
-    heapmem_free(te_p);
+    // heapmem_free(hu_p);
+    // heapmem_free(te_p);
+
+    memb_free(&th_buff, th_p);
 
     PROCESS_END();
 }
