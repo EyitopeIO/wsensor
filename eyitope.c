e02d0f052511a2da99359e8a2d35af4a89bfb2bc
EyitopeIO <adeyitope.io@gmail.com>
2021-11-12 16:11:12 +0100

    Updates.

--- eyitope.c
+++ eyitope.c
@@ -7,6 +7,7 @@
 #include "sys/clock.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "math.h"
 
 PROCESS(sense_and_send, "Main process");
 
@@ -16,8 +17,11 @@ PROCESS_THREAD(sense_and_send, ev, data)
 {
     LIST(quantum_tunnel_h);     // List of humidity readings
     LIST(quantum_tunnel_t);     // List of temperature readings
-    static struct sensorval_l hu_r;    // Humidity readings
-    static struct sensorval_l te_r;    // Temperature readings
+    static struct sensorval_l *hu_r;    // Humidity readings
+    static struct sensorval_l *te_r;    // Temperature readings
+    static struct sensorval_l *hu_p;
+    static struct sensorval_l *te_p;
+
     static struct etimer time_to_read;
     float avr_h;
     float avr_t;
@@ -28,38 +32,42 @@ PROCESS_THREAD(sense_and_send, ev, data)
     list_init(quantum_tunnel_h);
     list_init(quantum_tunnel_t);
 
-    // hu_r = (struct sensorval_l*)calloc(WINDOW_SIZE, sizeof(struct sensorval_l));
-    // te_r = (struct sensorval_l*)calloc(WINDOW_SIZE, sizeof(struct sensorval_l));
-    //if (hu_r == NULL || te_r == NULL) PROCESS_EXIT();
+    hu_p = (struct sensorval_l*)calloc(WINDOW_SIZE, sizeof(struct sensorval_l));
+    te_p = (struct sensorval_l*)calloc(WINDOW_SIZE, sizeof(struct sensorval_l));
+    if (hu_p == NULL || te_p == NULL) PROCESS_EXIT();
 
     avr_h = avr_t = 0.0f;
 
-    etimer_set(&time_to_read, CLOCK_SECOND * 30);
+    etimer_set(&time_to_read, CLOCK_SECOND * 8 );   // 1 mins / 7 secs
 
     SENSORS_ACTIVATE(sht11_sensor);
 
-
     while(1) {
 
-        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&time_to_read));
-
-        for (i=0; i < WINDOW_SIZE; i++) {
-            hu_r.reading = sht11_sensor.value(SHT11_SENSOR_HUMIDITY);
-            te_r.reading = sht11_sensor.value(SHT11_SENSOR_TEMP);
-            list_add(quantum_tunnel_h, &hu_r);
-            list_add(quantum_tunnel_t, &te_r);
+        for (i=0,hu_r=hu_p,te_r=te_p; i < WINDOW_SIZE; i++,hu_r++,te_r++) {
+            PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&time_to_read));
+            hu_r->reading = (float)sht11_sensor.value(SHT11_SENSOR_HUMIDITY);
+            te_r->reading = (float)sht11_sensor.value(SHT11_SENSOR_TEMP);
+            list_add(quantum_tunnel_h, hu_r);
+            list_add(quantum_tunnel_t, te_r);
+            etimer_reset(&time_to_read);
         }
 
-        avr_h = (float)osw_average(&quantum_tunnel_h);
-        avr_t = (float)osw_average(&quantum_tunnel_t);
+        avr_h = (0.01 * osw_average(&quantum_tunnel_h)) - 36.9;
+        avr_h = (0.01 * avr_h) - 36.9;
 
-        printf("avg humidity: %f\n", avr_h);
-        printf("avg temperature: %f\n", avr_t);
+        avr_t = osw_average(&quantum_tunnel_t);
+        avr_t = -4 + (0.0405 * avr_t) - ((2.8 * pow(10, -6)) * pow(avr_t, 2));
+
+        // printf("avg humidity: %f\n", avr_h);
+        // printf("avg temperature: %f\n", avr_t);
 
         // Send over network here;
 
-        etimer_reset(&time_to_read);
     }
+
+    free(hu_p);
+    free(te_p);
 
     PROCESS_END();
 }
