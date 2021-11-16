8664683cc2248a2d0ea87de20128f4b4373fcba8
Eyitope IO <adeyitope.io@gmail.com>
2021-11-16 10:57:52 +0100

    Update eyitope.c

--- eyitope.c
+++ eyitope.c
@@ -7,9 +7,32 @@
 #include "sys/clock.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include <string.h>
 #include "random.h"
 #include "math.h"
 #include "memb.h"
+
+#include "lib/random.h"
+#include "net/ip/uip.h"
+#include "net/ipv6/uip-ds6.h"
+#include "net/ip/uip-udp-packet.h"
+#ifdef WITH_COMPOWER
+#include "powertrace.h"
+#endif
+#include <string.h>
+
+#include "dev/serial-line.h"
+#include "net/ipv6/uip-ds6-route.h"
+
+
+
+static struct sensorval_l *th_r;    // Humidity & temperature readings 
+static struct sensorval_l *th_p;    // Pointer to block of memory
+static struct etimer time_to_read;
+static float avr_h;     // Average humidity
+static float avr_t;     // Average temperature
+static int counter;  
+
 
 PROCESS(sense_and_send, "Main process");
 
@@ -17,90 +40,52 @@ AUTOSTART_PROCESSES(&sense_and_send);
 
 PROCESS_THREAD(sense_and_send, ev, data) 
 {
-
-    static struct sensorval_l *th_r;    // Humidity readings
-    // static struct sensorval_l *te_r;    // Temperature readings
-    // static struct sensorval_l *hu_p;
-    // static struct sensorval_l *te_p;
-    static struct sensorval_l *th_p;
-    
-
-    static struct etimer time_to_read;
-    static float avr_h;
-    static float avr_t;
-    static int counter;
-
     PROCESS_BEGIN();
 
-    // LIST(quantum_tunnel_h);     // List of humidity readings
-    // LIST(quantum_tunnel_t);     // List of temperature readings
-    LIST(quantum_l);
-    MEMB(th_buff, struct sensorval_l, WINDOW_SIZE);
-
+    LSIST(quantum_l);        // List of acquired data points
+    MEMB(th_buff, struct sensorval_l, WINDOW_SIZE);     // Block of memory
     random_init(3);
-
-    // list_init(quantum_tunnel_h);
-    // list_init(quantum_tunnel_t);
     list_init(quantum_l);
     memb_init(&th_buff);
-
-    th_p = (struct sensorval_l*)memb_alloc(&th_buff);
-
-    // hu_p = (struct sensorval_l*)heapmem_alloc(WINDOW_SIZE * sizeof(struct sensorval_l));
-    // te_p = (struct sensorval_l*)heapmem_alloc(WINDOW_SIZE * sizeof(struct sensorval_l));
-    // hu_p = (struct sensorval_l*)heapmem_alloc(WINDOW_SIZE * sizeof(struct sensorval_l));
-    // te_p = (struct sensorval_l*)heapmem_alloc(WINDOW_SIZE * sizeof(struct sensorval_l));
-
-    // if (hu_p == NULL || te_p == NULL) {
-    //     printf("Not enough memory!\n");
-    //     PROCESS_EXIT();
-    // }
-
-
-    if (th_p == NULL) {
+    if((th_p = (struct sensorval_l*)memb_alloc(&th_buff)) == NULL) {
         printf("Not enough memory!\n");
         PROCESS_EXIT();
     }
-
     avr_h = avr_t = 0.0f;
-
-    etimer_set(&time_to_read, CLOCK_SECOND);   // 1 mins / 7 secs
+    etimer_set(&time_to_read, CLOCK_SECOND * 10);   // 1 mins / 7 secs
 
     // SENSORS_ACTIVATE(sht11_sensor);
-    
     printf("Sensor activated!\n");
 
     while(1) {
-
         printf("Loop!\n");
-
-        // hu_r = hu_p;
-        // te_r = te_p;
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&time_to_read));
 
+        /*
+        * Read temperature values
+        */
         counter = WINDOW_SIZE;
         th_r = th_p;
         while (counter--) { 
-            // hu_r->reading = (float)sht11_sensor.value(SHT11_SENSOR_HUMIDITY);
-            // te_r->reading = (float)sht11_sensor.value(SHT11_SENSOR_TEMP);
             th_r->reading = abs((float)random_rand());
-            printf("th_r1: %f\n",th_r->reading);
-            // te_r->reading = (float)random_rand();
-            // list_add(quantum_tunnel_h, hu_r++);
-            // list_add(quantum_tunnel_t, te_r++);
+            // thr_r->reading = (float)sht11_sensor.value(SHT11_SENSOR_TEMP);
+            // printf("temperature readings: %f\n",th_r->reading);
             list_add(quantum_l, th_r++);
         }
         avr_h = (0.01 * osw_average(&quantum_l)) - 36.9;
         printf("avg1 humidity: %f\n",avr_h);
         avr_h = (0.01 * avr_h) - 36.9;
 
+        /*
+        * Read Humidity values
+        */
         counter = WINDOW_SIZE;
         th_r = th_p;
         while (counter--) { 
             th_r->reading = abs((float)random_rand());
-            printf("th_r2: %f\n",th_r->reading);
+            // thr_r->reading = (float)sht11_sensor.value(SHT11_SENSOR_HUMIDITY);
+            // printf("th_r2: %f\n",th_r->reading);
             list_add(quantum_l, th_r++); 
-            etimer_reset(&time_to_read);
         }
         avr_t = osw_average(&quantum_l);
         printf("avg1 temperature: %f\n",avr_t);
@@ -109,15 +94,10 @@ PROCESS_THREAD(sense_and_send, ev, data)
         printf("avg humidity: %f\n", avr_h);
         printf("avg temperature: %f\n", avr_t);
 
-
         // Send over network here;
 
         etimer_reset(&time_to_read);
-
     }
-
-    // heapmem_free(hu_p);
-    // heapmem_free(te_p);
 
     memb_free(&th_buff, th_p);
 
