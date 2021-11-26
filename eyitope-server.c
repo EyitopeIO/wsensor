117fa01eabe7d011d6538eb62060f6de7196ecee
EyitopeIO <adeyitope.io@gmail.com>
2021-11-26 14:04:48 +0000

    Looking for the bug.

--- eyitope-server.c
+++ eyitope-server.c
@@ -42,11 +42,7 @@
 
 static struct simple_udp_connection udp_conn;
 
-PROCESS(udp_server_process, "UDP server");
-AUTOSTART_PROCESSES(&udp_server_process);
-/*---------------------------------------------------------------------------*/
-static void
-udp_rx_callback(struct simple_udp_connection *c,
+static void udp_rx_callback(struct simple_udp_connection *c,
          const uip_ipaddr_t *sender_addr,
          uint16_t sender_port,
          const uip_ipaddr_t *receiver_addr,
@@ -63,6 +59,11 @@ udp_rx_callback(struct simple_udp_connection *c,
   simple_udp_sendto(&udp_conn, data, datalen, sender_addr);
 #endif /* WITH_SERVER_REPLY */
 }
+
+
+PROCESS(udp_server_process, "UDP server");
+AUTOSTART_PROCESSES(&udp_server_process);
+
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_server_process, ev, data)
 {
