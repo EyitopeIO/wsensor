16735c53ea830facae9f1c04e9dfaf6b91ffabc0
EyitopeIO <adeyitope.io@gmail.com>
2021-11-26 15:51:36 +0000

    Refractoring

--- eyitope-calc-test.c
+++ eyitope-calc-test.c
@@ -9,18 +9,19 @@
 * Assume window size is 7
 */
 UNIT_TEST_REGISTER(test_below_window_size, "Test when input below window size");
-UNIT_TEST_REGISTER(test_equals_window_size, "Test when exactly window elements");
-UNIT_TEST_REGISTER(test_above_window_size, "Test input above window elements");
+UNIT_TEST_REGISTER(test_equals_window_size, "Test when exactly window size");
+UNIT_TEST_REGISTER(test_above_window_size, "Test input above window size");
 
 static float avg = 0.0f;
-static float cfm[] = {5.0f, 5.5f, 6.0f, 6.5f, 7.0f, 7.5f, 8.0f, 9.0};
-static float tst[] = {5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f};
+static float cfm[] = {5.0f, 5.5f, 6.0f, 6.5f, 7.0f, 7.5f, 8.0f, 9.0};   // Expected results
+static float tst[] = {5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f};   // test cases
 
 UNIT_TEST(test_below_window_size)
 {
     LIST(worm);
     list_init(worm);
 
+    /* Test inputs */
     struct sensorval_l sv1;
     struct sensorval_l sv2; 
     struct sensorval_l sv3; 
@@ -35,10 +36,10 @@ UNIT_TEST(test_below_window_size)
 
     UNIT_TEST_BEGIN();
 
-    avg = osw_average(&worm); // Find average of first 3
+    avg = osw_average(&worm);
     // printf("avg <: %f\n", avg);
     UNIT_TEST_ASSERT(avg > 0.0f); 
-    UNIT_TEST_ASSERT(avg == cfm[2]);
+    UNIT_TEST_ASSERT(avg == cfm[2]);    // cfm[2] is the 3rd element
     
     UNIT_TEST_END();
 }
@@ -74,10 +75,10 @@ UNIT_TEST(test_equals_window_size)
 
     UNIT_TEST_BEGIN();
 
-    avg = osw_average(&worm); // Find average of first 3
+    avg = osw_average(&worm);
     // printf("avg =: %f\n", avg);
     UNIT_TEST_ASSERT(avg > 0); 
-    UNIT_TEST_ASSERT(avg == cfm[6]);
+    UNIT_TEST_ASSERT(avg == cfm[6]);    
     
     UNIT_TEST_END();
 }
