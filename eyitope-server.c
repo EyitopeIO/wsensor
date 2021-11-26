f0a6ba5e3659735d269d869bb30955ee791185a2
EyitopeIO <adeyitope.io@gmail.com>
2021-11-26 14:20:35 +0000

    Looking for the bug.

--- eyitope-server.c
+++ eyitope-server.c
@@ -42,23 +42,23 @@
 
 static struct simple_udp_connection udp_conn;
 
-// static void udp_rx_callback(struct simple_udp_connection *c,
-//          const uip_ipaddr_t *sender_addr,
-//          uint16_t sender_port,
-//          const uip_ipaddr_t *receiver_addr,
-//          uint16_t receiver_port,
-//          const uint8_t *data,
-//          uint16_t datalen)
-// {
+static void udp_rx_callback(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
+{
 //   LOG_INFO("Received request '%.*s' from ", datalen, (char *) data);
 //   LOG_INFO_6ADDR(sender_addr);
 //   LOG_INFO_("\n");
 // #if WITH_SERVER_REPLY
 //   /* send back the same string to the client as an echo reply */
 //   LOG_INFO("Sending response.\n");
-//   simple_udp_sendto(&udp_conn, data, datalen, sender_addr);
-// #endif /* WITH_SERVER_REPLY */
-// }
+  simple_udp_sendto(&udp_conn, data, datalen, sender_addr);
+//#endif /* WITH_SERVER_REPLY */
+}
 
 
 PROCESS(udp_server_process, "UDP server");
